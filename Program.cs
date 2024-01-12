40478e1cedf02730104adb386f370710b665f096
maheye <u277721157@byui.edu>
2024-01-12 12:52:24 +0800

    Learning Activity 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This Current Time is" + DateTime.Now);
+Console.WriteLine($"Current Date: {DateTime.Now.ToString("yyyy-MM-dd")}\nDays Until Next Christmas: {(new DateTime(DateTime.Now.Year, 12, 25) - DateTime.Now).Days}");
